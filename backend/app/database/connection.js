77f2bb48884cd54f4bc3332e38d77969c4de9642
Phat Huynh <s3926661@rmit.edu.vn>
2023-05-01 11:05:13 +0700

    Update the frontend source files to contain only the neceessary html files, while keep all the css

--- backend/app/database/connection.js
+++ backend/app/database/connection.js
@@ -2,18 +2,23 @@ const mysql = require('mysql2');
 
 const con = mysql.createConnection({
     // host = RDS endpoint
-    // host: """,
+    host: "",
     
-    // user: "",
-    // password: "",
-    // port: 3000
+     user: "",
+     password: "",
+     port: 3000
 
     // Test With Local Database
 });
 
 con.connect(function(err) {
     if (err) throw err;
+    
     console.log('Connected to database');
 });
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
 
 module.exports = con;
