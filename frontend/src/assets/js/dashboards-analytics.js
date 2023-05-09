66aa7e0da30f0ab9ed8bdfb13b624af8bb196eea
Phat Huynh <s3926661@rmit.edu.vn>
2023-05-09 19:37:31 +0700

    Clean up the placeholder

--- frontend/src/assets/js/dashboards-analytics.js
+++ frontend/src/assets/js/dashboards-analytics.js
@@ -18,19 +18,19 @@
     totalRevenueChartOptions = {
       series: [
         {
-          name: 'Class A',
+          name: 'Course A',
           data: [18, 7, 15]
         },
         {
-          name: 'Class B',
+          name: 'Course B',
           data: [20, 16, 30]
         },
         {
-          name: 'Class C',
+          name: 'Course C',
           data: [5, 18, 6]
         },
         {
-          name: 'Class D',
+          name: 'Course D',
           data: [10, 15, 20]
         }
       ],
@@ -85,7 +85,7 @@
         }
       },
       xaxis: {
-        categories: ['Semester A', 'Semester B', 'Semester C'],
+        categories: ['Week 1', 'Week 2', 'Week 3'],
         labels: {
           style: {
             fontSize: '13px',
@@ -287,6 +287,279 @@
     const totalRevenueChart = new ApexCharts(totalRevenueChartEl, totalRevenueChartOptions);
     totalRevenueChart.render();
   }
+  ////////////////////////////////////////////////
+  //Display top 3 teachers and their respective top 3 students in terms of number of attendances
+  const topTeacherAttendance = document.querySelector('#topTeacherAttendance'),
+  topTeacherAttendanceOptions = {
+    series: [
+      {
+        name: 'Top 1',
+        data: [18, 7, 15]
+      },
+      {
+        name: 'Top 2',
+        data: [20, 16, 30]
+      },
+      {
+        name: 'Top 3',
+        data: [5, 18, 6]
+      },
+    
+    ],
+    chart: {
+      height: 300,
+      type: 'bar',
+      toolbar: { show: false }
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '33%',
+        borderRadius: 12,
+        startingShape: 'rounded',
+        endingShape: 'rounded'
+      }
+    },
+    colors: [config.colors.primary, config.colors.info, config.colors.black],
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'smooth',
+      width: 5,
+      lineCap: 'round',
+      colors: [cardColor]
+    },
+    legend: {
+      show: true,
+      horizontalAlign: 'left',
+      position: 'top',
+      markers: {
+        height: 8,
+        width: 8,
+        radius: 12,
+        offsetX: -3
+      },
+      labels: {
+        colors: axisColor
+      },
+      itemMargin: {
+        horizontal: 10
+      }
+    },
+    grid: {
+      borderColor: borderColor,
+      padding: {
+        top: 0,
+        bottom: -8,
+        left: 20,
+        right: 20
+      }
+    },
+    xaxis: {
+      categories: ['Teacher A', 'Teacher B', 'Teacher C'],
+      labels: {
+        style: {
+          fontSize: '13px',
+          colors: axisColor
+        }
+      },
+      axisTicks: {
+        show: false
+      },
+      axisBorder: {
+        show: false
+      }
+    },
+    yaxis: {
+      labels: {
+        
+        style: {
+          fontSize: '13px',
+          colors: axisColor,
+    
+        }
+      },
+      title: {
+        display: true,
+        text: 'Number of students\' attendance taken'
+      }
+    },
+  
+    responsive: [
+      {
+        breakpoint: 1700,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '32%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 1580,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '35%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 1440,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '42%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 1300,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '48%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 1200,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '40%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 1040,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 11,
+              columnWidth: '48%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 991,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '30%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 840,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '35%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 768,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '28%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 640,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '32%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 576,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '37%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 480,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '45%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 420,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '52%'
+            }
+          }
+        }
+      },
+      {
+        breakpoint: 380,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 10,
+              columnWidth: '60%'
+            }
+          }
+        }
+      }
+    ],
+    states: {
+      hover: {
+        filter: {
+          type: 'none'
+        }
+      },
+      active: {
+        filter: {
+          type: 'none'
+        }
+      }
+    }
+  };
+if (typeof topTeacherAttendance!== undefined && topTeacherAttendance !== null) {
+  const topTA = new ApexCharts(topTeacherAttendance, topTeacherAttendanceOptions);
+  topTA.render();
+}
+///////////////////////////////////////////////
 
   // Growth Chart - Radial Bar Chart
   // --------------------------------------------------------------------
