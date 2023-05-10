2589acfb5cacd1dc4c18305f5e51ebdfb65956ab
phamkiendinh0 <74864852+phamkiendinh0@users.noreply.github.com>
2023-05-10 15:31:21 +0700

    Done two queries for dashboard

--- backend/app/controller/attendance_controller.js
+++ backend/app/controller/attendance_controller.js
@@ -57,7 +57,7 @@ function getTeacherCourseStudentAttendanceByDate(req,res) {
 }
 
 function getTeacherCourseStudentAbsentByDate(req,res) {
-    Attendance.getTeacherCourseStudentAttendanceByDate(req.body, function(response) {
+    Attendance.getTeacherCourseStudentAbsentByDate(req.body, function(response) {
         res.send({result: response});
     });
 }
